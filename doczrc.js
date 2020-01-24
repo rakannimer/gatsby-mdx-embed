78fda813f65e2a8e378a3c47707d31c232e3921b
rakannimer <rakannimer@gmail.com>
2020-01-24 19:30:13 +0200

    initial

---
+++ doczrc.js
@@ -0,0 +1,12 @@
+export default {
+  typescript: true,
+  title: 'gatsby-mdx-embed',
+  description:
+    'gatsby-mdx-embed is an mdx plugin to embed YouTube, Twitter, Instagram etc into .mdx without imports',
+  base: 'https://gatsby-mdx-embed.netlify.com/',
+  ogImage: 'mdx-embed-main-og-image.jpg',
+  menu: ['Getting Started'],
+  themeConfig: {
+    initialColorMode: 'light'
+  }
+}
